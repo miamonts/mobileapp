2542c5efe02c68d49abfaae4851ea3936b42a644
miamonts <montuyamia07@gmail.com>
2021-04-20 16:05:05 +0800

    Update database.dart

--- mobileapp/lib/database.dart
+++ mobileapp/lib/database.dart
@@ -1,12 +1,12 @@
 import 'package:http/http.dart' as http;
 //import 'package:new_post/quizpage.dart';
 
-signup(username, password, finalScore) async {
+signup(username, password, score) async {
   var posturl = 'https://flutter-database.herokuapp.com/users';
   var response = await http.post(Uri.parse(posturl), body: {
     "username": username,
     "password": password,
-    "score": finalScore,
+    "score": score,
   });
   print(response.body);
 }
